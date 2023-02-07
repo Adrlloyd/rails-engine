cffd38ca479a4f137f0162c47188bee3d2a0014b
Alastair Lloyd <dex8056@aol.com>
2023-02-07 08:19:28 -0700

    initial set up

---
+++ spec/spec_helper.rb
@@ -0,0 +1,18 @@
+equire 'simplecov'
+SimpleCov.start 'rails'
+SimpleCov.add_filter ['spec', 'config']
+
+RSpec.configure do |config|
+ 
+  config.expect_with :rspec do |expectations|
+ 
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+end
